b723acf9306f57100f07d4c95f799f7a7824a376
Marcin Makarewicz <marcin@makarewicz.eu>
2021-01-23 13:16:33 +0100

    use intl-messageformat

---
+++ packages/translations/src/index.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import IntlMessageFormat from "intl-messageformat";
+import marked from "marked";
+
+export const DEFAULT_LOCALES = "pl-PL";
+
+const messages = {
+  "#article": {
+    "en-US": "# Header\n\nParagraph\nParagraph\n\nParagraph\nParagraph",
+    "pl-PL": "# Nagłówek\n\nParagraf\nParagraf\n\nParagraf\nParagraf",
+  },
+  "#lang": {
+    "en-US": "Choose language",
+    "pl-PL": "Wybierz język",
+  },
+  "#linga": {
+    "en-US": "Linga",
+    "pl-PL": "Linga",
+  },
+  "#photos": {
+    "en-US":
+      "You have {numPhotos, plural,\n      =0 {no photos.}\n      =1 {one photo.}\n      other {# photos.}\n    }",
+    "pl-PL":
+      "{numPhotos, plural,\n          =0 {Nie masz zdjęć.}\n          one {Masz jedno zdjęcie.}\n          few {Masz # zdjęcia.}\n          many {Masz # zdjęć.}\n          other {Masz # zdjęcia.}\n        }",
+  },
+};
+
+// https://formatjs.io/docs/intl-messageformat/
+const translate = (message: string, values: object, locales: string) =>
+  new IntlMessageFormat(message, locales).format(values);
+
+const addLocalesMessage = (
+  id: string,
+  locales: string,
+  defaultMessage: string
+) => {
+  if (!messages[id]) {
+    messages[id] = {};
+  }
+  if (!messages[id][locales]) {
+    Object.assign(messages[id], {
+      [locales]: defaultMessage,
+    });
+    console.log(Object.fromEntries(Object.entries(messages).sort()));
+  }
+  return messages[id][locales];
+};
+
+const getLocalesMessage = (
+  id: string,
+  locales: string,
+  defaultMessage: string
+) =>
+  (messages[id] && messages[id][locales]) ||
+  addLocalesMessage(id, locales, defaultMessage);
+
+export const useTranslation = (locales: string = DEFAULT_LOCALES) => (
+  message: string,
+  values: object,
+  id: string
+) => translate(getLocalesMessage(id, locales, message), values, locales);
+
+export default ({
+  children: message,
+  component: Component = "div",
+  id,
+  locales = DEFAULT_LOCALES,
+}: {
+  children: string;
+  component: React.ElementType;
+  id: string;
+  locales: string;
+}) => (
+  <Component
+    dangerouslySetInnerHTML={{
+      __html: marked(
+        translate(getLocalesMessage(id, locales, message), locales)
+      ),
+    }}
+  />
+);
--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,12 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import history from "history/browser";
+import T, { DEFAULT_LOCALES, useTranslation } from "@dev/translations";
+
+const getLocales = (location: { hash: string }) => {
+  const [path, hash = DEFAULT_LOCALES] =
+    decodeURI(location.hash).match(/^#(.+)/) || [];
+  console.log();
+  return hash;
+};
 
 export default () => {
+  const [locales, setLocales] = useState(getLocales(history.location));
+  const [numPhotos, setNumPhotos] = useState(2);
+  const t = useTranslation(locales);
+
+  useEffect(() =>
+    // location is an object like window.location
+    history.listen(({ location, action, ...rest }) =>
+      setLocales(getLocales(location))
+    )
+  );
   return (
-    <div>
-      <h1>Linga</h1>
-      <div>
-        lang{" "}
-        {["pl", "en"]
+    <section>
+      <h1>
+        <T id="#linga" {...{ locales }}>
+          Linga
+        </T>
+      </h1>
+      <nav>
+        <span>{t("Wybierz język", null, "#lang")}</span>
+        {` `}
+        {["pl-PL", "en-US"]
           .reduce(
             (list, item) => list.concat(list.length ? [null] : [], [item]),
             []
@@ -20,7 +44,39 @@ export default () => {
               ` | `
             )
           )}
-      </div>
-    </div>
+      </nav>
+      <T component="article" id="#article" {...{ locales }}>{`# Nagłówek
+
+Paragraf
+Paragraf
+
+Paragraf
+Paragraf`}</T>
+      <label>
+        <span>numPhotos</span>
+        <select
+          value={numPhotos}
+          onChange={(e) => setNumPhotos(e.target.value)}
+        >
+          {[0, 1, 2, 5, 10, 20, 21, 22, 100, 101, 102].map((value, key) => (
+            <option key={key} value={value}>
+              {value}
+            </option>
+          ))}
+        </select>
+      </label>
+      <p>
+        {t(
+          `You have {numPhotos, plural,
+      =0 {no photos.}
+      =1 {one photo.}
+      other {# photos.}
+    }`,
+          { numPhotos },
+          "#photos"
+        )}
+      </p>
+      <pre>{JSON.stringify({ locales, numPhotos }, null, 2)}</pre>
+    </section>
   );
 };
